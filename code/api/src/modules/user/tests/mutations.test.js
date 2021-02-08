1f9c37b4266981cff3f92cd7749a41566f1b8fdf
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-02-08 16:59:05 -0700

    Add user mutations test
    Co-Authored-By: Austin Aspaas <araspaas@live.com>
    Co-Authored-By: Eduardo Parra <helloeduardo@users.noreply.github.com>
    Co-Authored-By: null <Todd-Estes@users.noreply.github.com>

---
+++ code/api/src/modules/user/tests/mutations.test.js
@@ -0,0 +1,51 @@
+import request from 'supertest';
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import models from '../../../setup/models';
+import schema from '../../../setup/schema';
+import * as mockData from '../../mocks/index'
+
+describe("user mutations", () => {
+  let server;
+
+  beforeAll(() => {
+    server = express(); //sets up the server
+
+    server.use(
+      '/',
+      graphqlHTTP({
+        schema: schema,
+        graphiql: false
+      })
+    );
+  });
+
+  //Creates our users via the imported mocks/index file
+  beforeAll(async () => {
+    await models.User.create(mockData.userData1);
+    await models.User.create(mockData.userData2);
+  });
+
+  //deletes all users
+  afterAll(async () => {
+    await models.User.destroy({ where: {} })
+    server.close();
+  });
+
+  it("can update a user's attributes", async () => {
+    const response = await request(server)
+    .post('/')
+    .send({ query: 'mutation { userUpdate(id: 2, city: "Kenai", state: "Alaska"){ name image email description streetAddress city state zip country }}'})
+    .expect(200)
+
+    expect(response.body.data.userUpdate.name).toEqual('The User')
+    expect(response.body.data.userUpdate.image).toEqual('https://ichef.bbci.co.uk/news/976/cpsprodpb/CFE3/production/_108391235_nessie.jpg')
+    expect(response.body.data.userUpdate.email).toEqual('user@crate.com')
+    expect(response.body.data.userUpdate.description).toEqual('Nessie')
+    expect(response.body.data.userUpdate.streetAddress).toEqual('987 User St')
+    expect(response.body.data.userUpdate.city).toEqual('Kenai')
+    expect(response.body.data.userUpdate.state).toEqual('Alaska')
+    expect(response.body.data.userUpdate.zip).toEqual('80123')
+    expect(response.body.data.userUpdate.country).toEqual('USA')
+  });
+});
