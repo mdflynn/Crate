c5424aa51854c002b36a54408ff70bd19096de4e
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-02-09 15:41:54 -0700

    Uncomment test connection

--- code/api/src/setup/database.js
+++ code/api/src/setup/database.js
@@ -15,16 +15,16 @@ const connection = new Sequelize(databaseConfigEnv.database, databaseConfigEnv.u
   logging: false
 })
 
-// // Test connection
-// console.info('SETUP - Connecting database...')
-//
-// connection
-//   .authenticate()
-//   .then(() => {
-//     console.info('INFO - Database connected.')
-//   })
-//   .catch(err => {
-//     console.error('ERROR - Unable to connect to the database:', err)
-//   })
+// Test connection
+console.info('SETUP - Connecting database...')
+
+connection
+  .authenticate()
+  .then(() => {
+    console.info('INFO - Database connected.')
+  })
+  .catch(err => {
+    console.error('ERROR - Unable to connect to the database:', err)
+  })
 
 export default connection
