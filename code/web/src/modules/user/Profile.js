ee461d008a1ef040929e4ed888bc3d45c5915f12
mdflynn <mdflynn34@outlook.com>
2021-02-08 18:41:10 -0700

    Add general delivery structure

--- code/web/src/modules/user/Profile.js
+++ code/web/src/modules/user/Profile.js
@@ -9,9 +9,11 @@ import { Link } from "react-router-dom";
 import { Grid, GridCell } from "../../ui/grid";
 import { H3, H4 } from "../../ui/typography";
 import Button from "../../ui/button";
-import { grey, grey2 } from "../../ui/common/colors";
+import { white, grey, grey2, black } from "../../ui/common/colors";
 import { Tile } from "../../ui/image";
-import { Card } from "../../ui/card";
+import Card from "../../ui/card";
+import { level1 } from "../../ui/common/shadows";
+
 // App Imports
 import userRoutes from "../../setup/routes/user";
 import { logout } from "./api/actions";
@@ -32,33 +34,121 @@ const Profile = (props) => (
     </Grid>
 
     <Grid>
-      <GridCell>
-        <Tile
-          image="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg"
-          width="300px"
-          height="300px"
-          shadow="5px"
-        ></Tile>
-        <H3 font="secondary">User Description</H3>
-      </GridCell>
-      <GridCell>
-        <Tile
-          image="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg"
-          width="300px"
-          height="300px"
-          shadow="5px"
-        ></Tile>
-      </GridCell>
-    </Grid>
-
-    <Grid>
       <GridCell style={{ padding: "2em", textAlign: "center" }}>
         <H4 style={{ marginBottom: "0.5em" }}>{props.user.details.name}</H4>
 
         <p style={{ color: grey2, marginBottom: "2em" }}>
           {props.user.details.email}
         </p>
+      </GridCell>
+    </Grid>
 
+    <Grid>
+      <GridCell>
+        <Tile
+          image="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg"
+          width={250}
+          height={250}
+          shadow={level1}
+        ></Tile>
+        <H3 font="secondary">User Description</H3>
+      </GridCell>
+      <GridCell>
+        <Tile
+          image="https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg"
+          width={250}
+          height={250}
+          shadow={level1}
+        ></Tile>
+      </GridCell>
+    </Grid>
+
+    <Grid style={{ padding: "2em" }}>
+      <GridCell style={{ padding: "2em" }}>
+        {/* create function to generate cards */}
+
+        <Card
+          style={{ display: "flex", width: "80em", backgroundColor: white }}
+        >
+          <div style={{ padding: "1em 1.2em" }}>
+            <H4 font="secondary" style={{ color: black }}>
+              CRATE INFO
+            </H4>
+            <p style={{ color: grey2, marginTop: "1em" }}>More Info</p>
+          </div>
+          <div style={{ padding: "1em 1.2em" }}>
+            <H3 font="secondary">Deliverd on</H3>
+            <H3 font="secondary">Items</H3>
+            <H3 font="secondary">Status</H3>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "1em 1.2em",
+            }}
+          >
+            <Button theme="primary">KEEP Items</Button>
+            <Button theme="secondary">RETURN Items</Button>
+          </div>
+        </Card>
+
+        <Card
+          style={{ display: "flex", width: "80em", backgroundColor: white }}
+        >
+          <div style={{ padding: "1em 1.2em" }}>
+            <H4 font="secondary" style={{ color: black }}>
+              CRATE INFO
+            </H4>
+            <p style={{ color: grey2, marginTop: "1em" }}>More Info</p>
+          </div>
+          <div style={{ padding: "1em 1.2em" }}>
+            <H3 font="secondary">Deliverd on</H3>
+            <H3 font="secondary">Items</H3>
+            <H3 font="secondary">Status</H3>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "1em 1.2em",
+            }}
+          >
+            <Button theme="primary">KEEP Items</Button>
+            <Button theme="secondary">RETURN Items</Button>
+          </div>
+        </Card>
+
+        <Card
+          style={{ display: "flex", width: "80em", backgroundColor: white }}
+        >
+          <div style={{ padding: "1em 1.2em" }}>
+            <H4 font="secondary" style={{ color: black }}>
+              CRATE INFO
+            </H4>
+            <p style={{ color: grey2, marginTop: "1em" }}>More Info</p>
+          </div>
+          <div style={{ padding: "1em 1.2em" }}>
+            <H3 font="secondary">Deliverd on</H3>
+            <H3 font="secondary">Items</H3>
+            <H3 font="secondary">Status</H3>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              padding: "1em 1.2em",
+            }}
+          >
+            <Button theme="primary">KEEP Items</Button>
+            <Button theme="secondary">RETURN Items</Button>
+          </div>
+        </Card>
+      </GridCell>
+    </Grid>
+
+    <Grid style={{ padding: "2em", textAlign: "center" }}>
+      <GridCell>
         <Link to={userRoutes.subscriptions.path}>
           <Button theme="primary">Subscriptions</Button>
         </Link>
@@ -89,3 +179,20 @@ function profileState(state) {
 }
 
 export default connect(profileState, { logout })(Profile);
+
+{
+  /* <Card style={{ width: "18em", backgroundColor: white }}>
+<p style={{ padding: "2em 3em 0 3em" }}>
+  <img
+    src='https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg'
+    alt="test alt"
+    style={{ width: "100%" }}
+  />
+</p>
+<div style={{ padding: "1em 1.2em" }}>
+  <H4 font="secondary" style={{ color: black }}>
+    Men's Clothes
+  </H4>
+</div>
+</Card> */
+}
