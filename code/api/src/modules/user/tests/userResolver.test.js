4111262ed3b4e0d4c2c6470bd5d1e887bb83c705
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2021-02-09 11:40:06 -0700

    Remove uneeded code and whitespace

--- code/api/src/modules/user/tests/userResolver.test.js
+++ code/api/src/modules/user/tests/userResolver.test.js
@@ -12,54 +12,45 @@ import authentication from "../../../setup/authentication";
 //mock data
 import * as mockData from '../../mocks/index'
 
-
 describe("user-- resolver update", () => {
 
-    let server;
-    let token;
-    let admin1;
-    let user1;
+  let server;
+  let token;
+  let admin1;
+  let user1;
 
-    beforeAll(async () => {
+  beforeAll(async () => {
 
-      admin1 = await models.User.create(mockData.userData1);
-      user1 = await models.User.create(mockData.userData2);
+    admin1 = await models.User.create(mockData.userData1);
+    user1 = await models.User.create(mockData.userData2);
 
-      server = express();
-      server.use(authentication);
-      server.use(
-          '/',
-          graphqlHTTP({
-              schema,
-              graphiql: false,
-              context: {
-                  auth: {
-                      user: user1
-                  }
-              }
-          })
-      );
+    server = express();
+    server.use(authentication);
+    server.use(
+        '/',
+        graphqlHTTP({
+            schema,
+            graphiql: false,
+            context: {
+                auth: {
+                    user: user1
+                }
+            }
+        })
+    );
+  })
 
-      //log in & set up auth
-      const response = await request(server)
-          .post('/')
-          .send({ query: '{ userLogin(email: "user@crate.com", password: "123456890") { token }}'})
-          .expect(200)
+  afterAll(async ()=>{
+      await models.User.destroy({ where: {} })
+  })
 
-      token = response.body.data.userLogin.token
-    })
+  it("throws error if not a logged in user", async(done) => {
+    const response = await request(server)
+    .post('/')
+    .send({ query: 'mutation { userUpdate(city: "Kenai", state: "Alaska"){ name image email description streetAddress city state zip country }}'})
+    .expect(200)
 
-    afterAll(async ()=>{
-        await models.User.destroy({ where: {} })
-    })
-
-    it("throws error if not a logged in user", async(done) => {
-      const response = await request(server)
-      .post('/')
-      .send({ query: 'mutation { userUpdate(city: "Kenai", state: "Alaska"){ name image email description streetAddress city state zip country }}'})
-      .expect(200)
-
-      expect(response.body.errors[0].message).toEqual('Please login to update information.')
-      done();
-    });
+    expect(response.body.errors[0].message).toEqual('Please login to update information.')
+    done();
   });
+});
