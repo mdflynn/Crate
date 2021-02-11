489a1761daa0bac6345a96ddf9b089a63226ade3
Eduardo Parra <helloeduardo@icloud.com>
2021-02-11 16:29:38 -0700

    Import orderProducts
    Co-Authored-By: Austin Aspaas <araspaas@live.com>
    Co-Authored-By: Jonathan Wilson <Jonathan-M-Wilson@users.noreply.github.com>
    Co-Authored-By: null <Todd-Estes@users.noreply.github.com>

--- code/api/src/setup/schema/queries.js
+++ code/api/src/setup/schema/queries.js
@@ -7,7 +7,7 @@ import * as product from '../../modules/product/query'
 import * as crate from '../../modules/crate/query'
 import * as subscription from '../../modules/subscription/query'
 import * as order from '../../modules/order/query'
-// import * as orderProduct from '../../modules/orderProduct/query'
+import * as orderProduct from '../../modules/orderProduct/query'
 
 // Query
 const query = new GraphQLObjectType({
@@ -19,8 +19,8 @@ const query = new GraphQLObjectType({
     ...product,
     ...crate,
     ...subscription,
-    ...order
-    // ...orderProduct
+    ...order,
+    ...orderProduct
   })
 })
 
