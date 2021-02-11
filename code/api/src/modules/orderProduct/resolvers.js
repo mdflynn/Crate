29ab3cace89daae94960c27f2fd66b594dd42011
Eduardo Parra <helloeduardo@icloud.com>
2021-02-11 16:29:09 -0700

    Add orderProduct resolvers
    Co-Authored-By: Austin Aspaas <araspaas@live.com>
    Co-Authored-By: Jonathan Wilson <Jonathan-M-Wilson@users.noreply.github.com>
    Co-Authored-By: null <Todd-Estes@users.noreply.github.com>

---
+++ code/api/src/modules/orderProduct/resolvers.js
@@ -0,0 +1,52 @@
+// App Imports
+import models from '../../setup/models'
+
+// Get orderProducts by ID
+export async function get(parentValue, { id }) {
+  return await models.OrderProduct.findOne({
+    where: { id },
+    include: [
+      { model: models.Product, as: 'product' },
+      { model: models.Order, as: 'order' }
+    ]
+  })
+}
+
+// Get orderProducts by user
+export async function getByUser(parentValue, { id }) {
+  return await models.sequelize.query(`SELECT * FROM "orders" INNER JOIN "subscriptions" ON "subscriptions".id = "orders".id WHERE "subscriptions"."userId" = ${id};`, {
+    model: models.OrderProduct,
+    mapToModel: true
+  })
+}
+
+// Get all orderProducts
+export async function getAll() {
+  return await models.OrderProduct.findAll({
+    include: [
+      { model: models.Product, as: 'product' },
+      { model: models.Order, as: 'order' }
+    ]
+  })
+}
+
+// Create order
+// export async function create(parentValue, { crateId }, { auth }) {
+//   if(auth.user && auth.user.id > 0) {
+//     return await models.OrderProduct.create({
+//       crateId,
+//       userId: auth.user.id
+//     })
+//   } else {
+//     throw new Error('Please login to subscribe to this crate.')
+//   }
+// }
+
+// Delete order
+// export async function remove(parentValue, { id }, { auth }) {
+//   if(auth.user && auth.user.id > 0) {
+//     return await models.OrderProduct.destroy({where: {id, userId: auth.user.id}})
+//   } else {
+//     throw new Error('Access denied.')
+//   }
+// }
