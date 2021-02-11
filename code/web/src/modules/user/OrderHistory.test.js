e5395f1d25e5a5129b0928bd2808ac5f8780a899
mdflynn <mdflynn34@outlook.com>
2021-02-10 20:16:42 -0700

    Add final unit tests

--- code/web/src/modules/user/OrderHistory.test.js
+++ code/web/src/modules/user/OrderHistory.test.js
@@ -4,10 +4,12 @@ import React from "react";
 import { Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
 import OrderHistory from "./OrderHistory";
-import { Provider } from "react-redux";
 import { store } from "../../setup/store";
+import { Provider } from "react-redux";
 
 describe("OrderHistory", () => {
+  let history;
+
   beforeEach(() => {
     store.user = {
       details: {
@@ -27,32 +29,106 @@ describe("OrderHistory", () => {
         status: "delivered",
       },
     };
-    const history = createMemoryHistory();
 
-    render(
-      <Router history={history}>
-        <OrderHistory data={store} />
-      </Router>
-    );
+    history = createMemoryHistory();
   });
 
   it("should render an OrderHistory", () => {
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <OrderHistory data={store} />
+        </Router>
+      </Provider>
+    );
     const crate = screen.getByRole("img");
     expect(crate).toBeInTheDocument();
   });
 
+  it("should display crate type", () => {
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <OrderHistory data={store} />
+        </Router>
+      </Provider>
+    );
+
+    const type = screen.getByText("Men's Accesories");
+    expect(type).toBeInTheDocument();
+  });
+
   it("should display a formatted date", () => {
-    const date = screen.getByRole('heading', { name: /delivered on: feb\/01\/2021/i });
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <OrderHistory data={store} />
+        </Router>
+      </Provider>
+    );
+
+    const date = screen.getByRole("heading", {
+      name: /delivered on: feb\/01\/2021/i,
+    });
     expect(date).toBeInTheDocument();
   });
 
   it("should display kept items", () => {
-    const keptItem = screen.getByText('Belt for Men');
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <OrderHistory data={store} />
+        </Router>
+      </Provider>
+    );
+
+    const keptItem = screen.getByText("Belt for Men");
     expect(keptItem).toBeInTheDocument();
   });
 
-  it('should not have returned items', () => {
-    const returnedItem = screen.queryByText('Watch for Men');
+  it("should not have returned items", () => {
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <OrderHistory data={store} />
+        </Router>
+      </Provider>
+    );
+
+    const returnedItem = screen.queryByText("Watch for Men");
     expect(returnedItem).not.toBeInTheDocument();
-  })
+  });
+
+  it("should have the delivery status", () => {
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <OrderHistory data={store} />
+        </Router>
+      </Provider>
+    );
+
+    const status = screen.getByRole("heading", { name: /status: delivered/i });
+    expect(status).toBeInTheDocument();
+  });
+
+  it("should display message if all items returned", () => {
+    store.user.details.orderProducts = [
+      {
+        name: "Watch for Men",
+        returned: true,
+      },
+    ];
+
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <OrderHistory data={store} />
+        </Router>
+      </Provider>
+    );
+
+    const allReturned = screen.getByText("No items kept from this order");
+    expect(allReturned).toBeInTheDocument();
+  });
 });
